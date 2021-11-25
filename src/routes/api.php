357953048a5c5cf956fa8a9ec752440ef2953bf0
Wojciech Jezowski <wojciech.jezowski@me.pl>
2021-11-25 17:02:22 +0100

    Default namespace change

--- src/routes/api.php
+++ src/routes/api.php
@@ -2,4 +2,12 @@
 
 use Illuminate\Support\Facades\Route;
 
-Route::get('health-check', '\App\Http\Controllers\HealthCheck@healthCheck');
+/**
+ * API routes like in laravel;
+ */
+Route::prefix('api')
+	->middleware('api')
+	->get(
+		'health-check',
+		\Wjezowski\LaravelEureka\Http\Controllers\HealthCheck::class . '@healthCheck'
+	);
